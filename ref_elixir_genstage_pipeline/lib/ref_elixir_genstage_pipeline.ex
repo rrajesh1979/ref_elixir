add394c80fa5dbbe21ea377f2a51080c8793876f
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-01-19 14:40:01 -0500

    :star: Initial commit Setting up projects for different ways to build a data processing pipeline

---
+++ ref_elixir_genstage_pipeline/lib/ref_elixir_genstage_pipeline.ex
@@ -0,0 +1,18 @@
+defmodule RefElixirGenstagePipeline do
+  @moduledoc """
+  Documentation for `RefElixirGenstagePipeline`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RefElixirGenstagePipeline.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
